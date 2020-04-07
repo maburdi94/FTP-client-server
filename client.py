ec3a33e3a2403b0c4bc332cf65012b3bd2f0292f
Michael Burdi <maburdi@gmail.com>
2020-04-07 16:45:23 -0700

    Add functions for client commands GET, PUT, LS and error

--- client.py
+++ client.py
@@ -5,14 +5,32 @@ from sys import exit, argv
 import socket
 
 
+
+def process_get(*tokens):
+	print("GET", *tokens)
+
+
+def process_put(*tokens):
+	print("PUT", *tokens)
+
+
+def process_ls(*tokens):
+	print("LS", *tokens)
+
+
+def process_error(*tokens):
+	print("ERROR", f"No command matched {tokens[0]} with arguments {tokens[1:]}")
+
+
+
 # Command line checks 
-if len(argv) < 2:
-	print "USAGE python " + argv[0] + " <PORT>" 
+if len(argv) < 3:
+	print(f"USAGE python {argv[0]} <ADDRESS> <PORT>") 
 	exit()
 
 
 # Server (Address, Port)
-server = ("localhost", int(argv[1]))
+server = (argv[1], int(argv[2]))
 
 
 # Create a TCP socket
@@ -21,10 +39,65 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Connect to the server
 s.connect(server)
 
-# Close connection
-s.close();
+print( "--------------------------------------------------------")
+print( "Connection success!")
+print(f"You are connected to {server[0]} on port {server[1]}.")
+print( "Type 'exit' to quit.")
+print( "--------------------------------------------------------")
 
 
-print(server)
+while True:
+
+	cmd = input("ftp> ")
+
+	if not cmd:
+		continue
+
+	if cmd == "exit":
+		break
+
+	cmd, *args = cmd.split()
+
+	switch = {
+		"GET": process_get,
+		"PUT": process_put,
+		"LS": process_ls
+	}
+
+	action = switch.get(cmd)
+
+	if action:
+		action(*args)
+	else:
+		process_error(cmd, *args)
+
+
+
+s.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
